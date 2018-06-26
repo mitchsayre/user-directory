118083411f84a96def78d01e0fc190496b036b33
Mitchell Sayre <sketch0306@gmail.com>
2018-06-26 18:36:38 -0400

    Change 2nd h1

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button')
+button.addEventListener('click', handleClick)
+
+function handleClick() {
+    const heading = document.querySelector('#dayHeading')
+    heading.textContent = 'click!'
+}
