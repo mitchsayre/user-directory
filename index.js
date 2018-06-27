05082351292329677464fd52b1eb958228492682
Mitchell Sayre <sketch0306@gmail.com>
2018-06-26 20:43:50 -0400

    Add multiple names, clear imput each time

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('form#userForm')
 const handleSubmit = function(ev) {
     ev.preventDefault()
-    const heading = document.querySelector('h1')
     const form = ev.target
+    const users = document.querySelector('#users')
     const userName = form.userName.value  
-    heading.textContent = userName
+    users.textContent += ' ' + userName
+    form.userName.value = ''
 }
 
 
