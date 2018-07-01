c881836e8173298e59207851f6188fecb28e855a
Mitchell Sayre <sketch0306@gmail.com>
2018-06-30 22:23:58 -0400

    Store the user's data in one object

--- index.js
+++ index.js
@@ -22,21 +22,34 @@ function renderListItem(label, value) {
 const handleSubmit = function(ev) {
     ev.preventDefault()
     const form = ev.target
+    const user = {
+        'Name': form.userName.value,
+        'Age': form.age.value,
+        'Favorite Color': renderColor(form.favoriteColor.value),
+    }
+
+    const list = document.createElement('ul')
+
+    Object.keys(user).forEach(function(label) {
+        const item = renderListItem(label, user[label])
+        list.appendChild(item)
+    })
+
     const age = form.age.value   
     const userName = form.userName.value 
     const favoriteColor = renderColor(form.favoriteColor.value) 
     
     const users = document.querySelector('#users') 
-    const list = document.createElement('ul')
+   
     
     const colorItem = document.createElement('li')
     colorItem.textContent = 'Favorite color: '
     colorItem.appendChild(renderColor(favoriteColor))
    
     
-    list.appendChild(renderListItem('Name', userName))
-    list.appendChild(renderListItem('Age', age))
-    list.appendChild(renderListItem('favorite Color', favoriteColor))
+    // list.appendChild(renderListItem('Name', userName))
+    // list.appendChild(renderListItem('Age', age))
+    // list.appendChild(renderListItem('favorite Color', favoriteColor))
     
 
     users.appendChild(list)
